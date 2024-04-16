a70d4794cb86ac39e429367e4ec73510d3a79a4c
Hameed <sd.hameed79@gmail.com>
2024-04-16 17:17:30 +0530

    Next Topic Added

---
+++ Arrays/Duplicate.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class Duplicate {
+    static boolean FindDuplicate(int[] arr) {
+        for(int i=0; i<arr.length; i++) {
+            for(int j=i+1; j<arr.length; j++) {
+                if(arr[i] == arr[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Main.java
+    public static void main(String[] args) {
+        int[] arr = {4, 7, 1, 3, 2, 7, 1};
+
+        // Function call
+        System.out.println(FindDuplicate(arr));
+    }
+}
